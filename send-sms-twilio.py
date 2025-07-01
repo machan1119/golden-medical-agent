003f782f6fb82c0f18ab32d1e564012283b15a7d
machan1119 <sl.highlight999@gmail.com>
2025-07-01 15:30:10 +0900

    fix: change send-sms test code

--- send-sms-twilio.py
+++ send-sms-twilio.py
@@ -1,8 +1,8 @@
+# This is just end sms to twilio phone number for testing.
+
 import os
 from twilio.rest import Client
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 client = Client(account_sid, auth_token)
